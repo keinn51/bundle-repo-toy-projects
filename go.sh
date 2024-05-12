f0b619d2c4e7a8afc7c4d959c079beffb1ae82fb
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-05-13 00:30:07 +0900

    토이프로젝트 서브모듈 추가

---
+++ go.sh
@@ -0,0 +1,8 @@
+git submodule add https://github.com/toy-project-kyungsle/make-movie-app.git make-movie-app
+git submodule add https://github.com/toy-project-kyungsle/clone-slack.git clone-slack
+git submodule add https://github.com/toy-project-kyungsle/make-react-boiler-plate.git make-react-boiler-plate
+git submodule add https://github.com/toy-project-kyungsle/clone-twitter.git clone-twitter
+git submodule add https://github.com/toy-project-kyungsle/make-board-client.git make-board-client
+git submodule add https://github.com/toy-project-kyungsle/make-board-server.git make-board-server
+git submodule add https://github.com/toy-project-kyungsle/make-drag-drop-puzzle.git make-drag-drop-puzzle
+git submodule add https://github.com/toy-project-kyungsle/make-extracting-string-program.git make-extracting-string-program
